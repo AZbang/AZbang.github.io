fabf730c2e3ff22eb746fd9c39c2dcf05f1050b4
azbang <azbango5@gmail.com>
2017-10-30 12:06:32 +0300

    production version

--- src/app.js
+++ src/app.js
@@ -1,36 +1,36 @@
 "use strict";
 
-const Handlebars = require('handlebars');
 const $ = require('jquery');
-
+const Handlebars = require('handlebars');
 const renderPoly = require('./renderPoly');
 
-// My Data
-const data = require('./data');
-data.descriptionMarkdown = data.description.join('\n\n');
-data.contactsMarkdown = data.contacts.join('\n\n');
-data.image = data.images[Math.floor(Math.random()*data.images.length)];
+Handlebars.registerHelper('markdown', require('helper-markdown'));
 
 $(() => {
-	Handlebars.registerHelper('markdown', require('helper-markdown'));
-	var source = $("#template").html();
-	var template = Handlebars.compile(source);
-	$('body').html(template(data));
+	$.getJSON("./data.json", (data) => {
+		data.descriptionMarkdown = data.description.join('\n\n');
+		data.contactsMarkdown = data.contacts.join('\n\n');
+		data.image = data.images[Math.floor(Math.random()*data.images.length)];
 
-	let img = document.createElement('img');
-	img.setAttribute('src', data.image)
+		var source = $("#template").html();
+		var template = Handlebars.compile(source);
+		$('body').html(template(data));
 
-	img.addEventListener('load', () => {
-		let colorThief = new ColorThief();
-		let main = colorThief.getColor(img);
-		let back = colorThief.getPalette(img)[1];
-		let maincolor = `rgb(${main[0]}, ${main[1]}, ${main[2]})`;
-		let backcolor = `rgb(${back[0]}, ${back[1]}, ${back[2]})`;
-		$('.avatar, .mui-panel').css('borderColor', maincolor);
-		$('.text-color-thief').css({'color': backcolor});
-		$('.bg-color-thief').css({'background-color': maincolor});
+		let img = document.createElement('img');
+		img.setAttribute('src', data.image)
 
-		if(window.innerWidth > 520)
-			renderPoly($('#background')[0], window.innerWidth, window.innerHeight, maincolor, backcolor);
+		img.addEventListener('load', () => {
+			let colorThief = new ColorThief();
+			let main = colorThief.getColor(img);
+			let back = colorThief.getPalette(img)[1];
+			let maincolor = `rgb(${main[0]}, ${main[1]}, ${main[2]})`;
+			let backcolor = `rgb(${back[0]}, ${back[1]}, ${back[2]})`;
+			$('.avatar, .mui-panel').css('borderColor', maincolor);
+			$('.text-color-thief').css({'color': backcolor});
+			$('.bg-color-thief').css({'background-color': maincolor});
+
+			if(window.innerWidth > 520)
+				renderPoly($('#background')[0], window.innerWidth, window.innerHeight, maincolor, backcolor);
+		});
 	});
 });

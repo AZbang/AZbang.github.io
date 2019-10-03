e4477bd4df73feed0565c2331d97d3872afc14a4
AZbang <azbango5@gmail.com>
2019-10-03 11:48:40 +0300

    add cv

--- js/blurBg.js
+++
@@ -1,9 +0,0 @@
-window.blurBg = function(viewId, image, w, h, blur) {
-  var view = document.getElementById(viewId);
-  view.width = w;
-  view.height = h;
-  var ctx = view.getContext('2d');
-
-  ctx.drawImage(image, 0, 0, w, h);
-  StackBlur.canvasRGB(view, 0, 0, view.width, view.height, blur);
-}
